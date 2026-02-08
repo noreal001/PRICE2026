686e5340db5614b6d6089e1464c85838b6d2a721
Dzhamal Dzhavadov <jamaljavadov@mail.ru>
2026-02-08 04:21:32 +0300

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: true
+  },
+  preview: {
+    host: true,
+    allowedHosts: ['railway.app', 'up.railway.app']
+  }
+})
